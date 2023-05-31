697ee51fc7de8011c31b85ab4e877626af40e118
jakeevans <ejake370@gmail.com>
2023-05-30 22:24:45 -0600

    Add (Index.js). Created middleware to automatically convert to json, and configure express Router.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.JSON);
 
 app.use(express.static("public"));
+
+var apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+app.use((_req, res) => {
+  res.sendFile("index.html", { root: "public" });
+});
 
 app.listen(PORT, () => console.log(`Connection active at port ${PORT}`));
