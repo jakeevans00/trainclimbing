2bb49f8e57dace2a3543b976b52dc5a5dcb349b9
jakeevans <ejake370@gmail.com>
2023-06-01 21:54:34 -0600

    ADD (getWorkout). Added DB function to get workouts from database (to populate training page)

--- database.js
+++ database.js
@@ -3,8 +3,9 @@ const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db("simon");
-const scoreCollection = db.collection("score");
+const db = client.db("startup");
+const userCollection = db.collection("user");
+const workoutCollection = db.collection("workout");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -17,19 +18,17 @@ const scoreCollection = db.collection("score");
   process.exit(1);
 });
 
-async function addScore(score) {
-  const result = await scoreCollection.insertOne(score);
+async function addWorkouts(workouts) {
+  const result = await workoutCollection.insertMany(workouts);
   return result;
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
+function getWorkouts() {
+  const cursor = workoutCollection.find();
   return cursor.toArray();
 }
 
-module.exports = { addScore, getHighScores };
+module.exports = {
+  addWorkouts,
+  getWorkouts,
+};
