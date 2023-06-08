c61d059e10773a29d46171a2e4ddcb2f5e50fa99
jakeevans <ejake370@gmail.com>
2023-06-08 15:00:41 -0700

    Websocket: Finished implementation. Chat is now finished in beta.js

--- public/static/js/beta.js
+++ public/static/js/beta.js
@@ -32,15 +32,6 @@ function logout() {
   }).then(() => (window.location.href = "/"));
 }
 
-// let chat = "Placeholder";
-// setInterval(() => {
-//   let parent = document.querySelector(".msg-area");
-//   let child = document.createElement("div");
-//   child.setAttribute("class", "msg");
-//   child.textContent = chat;
-//   parent.insertBefore(child, parent.children[0]);
-// }, 5000);
-
 //Functionality for Web Socket
 // Adjust the webSocket protocol to what is being used for HTTP
 const protocol = window.location.protocol === "http:" ? "ws" : "wss";
@@ -48,25 +39,20 @@ const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
 
 // Display that we have opened the webSocket
 socket.onopen = (event) => {
-  appendMsg("msg", "websocket", "connected");
+  appendMsg("system", "websocket", "connected");
 };
 
 // Display messages we receive from our friends
 socket.onmessage = async (event) => {
   const text = await event.data.text();
   const chat = JSON.parse(text);
-  let cls;
-  if (chat.name === JSON.parse(localStorage.getItem("userName"))) {
-    cls = "user";
-  } else {
-    cls = "msg";
-  }
-  appendMsg(cls, chat.name, chat.msg);
+
+  appendMsg("msg", chat.name, chat.msg);
 };
 
 // If the webSocket is closed then disable the interface
 socket.onclose = (event) => {
-  appendMsg("msg", "websocket", "disconnected");
+  appendMsg("system", "websocket", "disconnected");
   // document.querySelector("#name-controls").disabled = true;
   // document.querySelector("#chat-controls").disabled = true;
 };
@@ -83,6 +69,15 @@ function sendMessage() {
   }
 }
 
+function broadcastEvent(from, type, value) {
+  const event = {
+    from: from,
+    type: type,
+    value: value,
+  };
+  this.socket.send(JSON.stringify(event));
+}
+
 // Create one long list of messages
 function appendMsg(cls, from, msg) {
   const parent = document.querySelector(".msg-area");
@@ -91,14 +86,6 @@ function appendMsg(cls, from, msg) {
   child.innerText = `${from}: ${msg}`;
   parent.insertBefore(child, parent.children[0]);
 }
-
-// Send message on enter keystroke
-// const input = document.querySelector("#new-msg");
-// input.addEventListener("keydown", (e) => {
-//   if (e.key === "Enter") {
-//     sendMessage();
-//   }
-// });
 
 // Disable chat if no name provided
 // const chatControls = document.querySelector(".msg-area");
